ae14b836dd74a67c79d25a73e600e697b4ba097b
Oscar Stanford <oestanford@perse.co.uk>
2020-07-28 14:24:15 +0100

    Completed numered objectives 1: Must print a 4x4 grid of full stops onto the screen at the beginning of the game, and 13: P1 goes first

--- Game.py
+++ Game.py
@@ -8,11 +8,28 @@ class Game:
     EMPTY = " "
     DRAW = auto()
 
-    def __init__(self):
-        pass
+    def __init__(self,dim):
+        self._dim = dim
+        self._rowLines = [[self.EMPTY for _ in range(self._dim - 1)] for _ in range(self._dim)]
+        self._colLines = [[self.EMPTY for _ in range(self._dim)] for _ in range(self._dim - 1)]
+        self._boxes = [[self.EMPTY for _ in range(self._dim - 1)] for _ in range(self._dim - 1)]
+        self._player = self.P1
 
     def __repr__(self):
-        pass
+        printable = "\n  "
+        for i in range(self._dim):
+            printable += str(i+1) + "  "
+        for i in range(self._dim):
+            printable += f"\n{i+1} "
+            for p in self._rowLines[i]:
+                printable += "." + p.rjust(2)
+            printable += ".\n  "
+            try:
+                for p in range(len(self._colLines[i])):
+                    printable += self._colLines[i][p] + self._boxes[i][p].rjust(2)
+            except IndexError:
+                pass
+        return printable
 
     def play(self, fr, to):
         pass
