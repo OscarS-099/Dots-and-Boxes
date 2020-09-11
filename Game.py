14e3beddd0ff30ce10f4447409347f9af020f8d4
Oscar Stanford <oestanford@perse.co.uk>
2020-09-11 15:58:44 +0100

    Removed debugging print statements from previous commit, slightly edited input system and scoring variable. Completed numbered objectives 4: When a box is made from the inputted lines, must signify which player made it by printing a P1 or P2 in the box, 5: When a box is made from the inputted lines, must signify which player made it by printing a P1 or P2 in the box, 6: Must output which player wins - which player has more points - at the end of the game, and end the program, 7: The game is finished when all possible lines are placed, 8: A player scores a point by capturing a box, the player with the most points wins, 9: A player gets another turn if they capture a square and 10: A square is captured if four lines make a square, with no dots within; i.e. it has to be a 1x1 square

--- Game.py
+++ Game.py
@@ -16,8 +16,7 @@ class Game:
         self._colLines = [[self.EMPTY for _ in range(self._dim)] for _ in range(self._dim-1)]
         self._boxes = [[self.EMPTY for _ in range(self._dim-1)] for _ in range(self._dim-1)]
         self._player = self.P1
-        self._P1pts = 0
-        self._P2pts = 0
+        self._points = {self.P1:0, self.P2:0}
 
     def __repr__(self):
         printable = "\n  "
@@ -38,16 +37,74 @@ class Game:
         return printable
 
     def play(self, fr, to):
+        row = min(fr[1],to[1])
+        col = min(fr[0],to[0])
+        sides = []
         if fr[0] == to[0]:
-            self._colLines[min(fr[1],to[1])][fr[0]] = self.VTCL
+            self._colLines[row][col] = self.VTCL
+            if col < self._dim-1:
+                sides.append("r")
+            if col > 0:
+                sides.append("l")
         elif fr[1] == to[1]:
-            self._rowLines[fr[1]][min(fr[0],to[0])] = self.HZTL
-        print(self._colLines)
-        print(self._rowLines)
+            self._rowLines[row][col] = self.HZTL
+            if row < self._dim-1:
+                sides.append("d")
+            if row > 0:
+                sides.append("u")
+        self.checkBox(row, col, sides)
+
+    def checkBox(self, row, col, sides):
+        switch = []
+        for side in sides:
+            if side == "r" or side == "d":
+                if self._colLines[row][col] != self.EMPTY and self._colLines[row][col+1] != self.EMPTY and self._rowLines[row][col] != self.EMPTY and self._rowLines[row+1][col] != self.EMPTY:
+                    self._boxes[row][col] = self._player
+                    self._points[self._player] += 1
+                    switch.append(False)
+                else:
+                    switch.append(True)
+            elif side == "l":
+                if self._colLines[row][col] != self.EMPTY and self._colLines[row][col-1] != self.EMPTY and self._rowLines[row][col-1] != self.EMPTY and self._rowLines[row+1][col-1] != self.EMPTY:
+                    self._boxes[row][col-1] = self._player
+                    self._points[self._player] += 1
+                    switch.append(False)
+                else:
+                    switch.append(True)
+            elif side == "u":
+                if self._colLines[row-1][col] != self.EMPTY and self._colLines[row-1][col+1] != self.EMPTY and self._rowLines[row][col] != self.EMPTY and self._rowLines[row-1][col] != self.EMPTY:
+                    self._boxes[row-1][col] = self._player
+                    self._points[self._player] += 1
+                    switch.append(False)
+                else:
+                    switch.append(True)
+        if len(switch) == 1:
+            if switch[0]:
+                self._player = self.P1 if self._player == self.P2 else self.P2
+        else:
+            if switch[0] and switch[1]:
+                self._player = self.P1 if self._player == self.P2 else self.P2
 
     @property
     def winner(self):
-        pass
+        for i in range(self._dim):
+            try:
+                for line in self._rowLines[i]:
+                    if line == self.EMPTY:
+                        return None
+            except IndexError:
+                pass
+            try:
+                for line in self._colLines[i]:
+                    if line == self.EMPTY:
+                        return None
+            except IndexError:
+                pass
+        if self._points[self.P1] > self._points[self.P2]:
+            return self.P1
+        elif self._points[self.P1] < self._points[self.P2]:
+            return self.P2
+        return self.DRAW
 
 if __name__ == "__main__":
     # For unit testing
