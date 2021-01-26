e6fb0f8bdf4a79e77a5d5b71dc1f13a3c9664215
Chimera099 <oscar.stanford@icloud.com>
2021-01-26 08:51:08 +0000

    Completed numbered objectives 21: Pressing a button that is black should not cause the window to be updated and the player to forfeit their turn, 29:Pressing the 'Help' button should open a new window that should display the rules and have a 'Dismiss' button, 30:Pressing the 'Dismiss' button in the help window should cause the help window to disappear, 31:When the 'Play Game' button is pressed, an options window is opened where the user is prompted to input the grid size using a dropdown box for dimension (square board), 32:A 'Play' button which dismisses the option window and opens the game window with the options choses should be at the bottom of the options window, 33:There should be a running total of how many squares each player has, 34:In the options window, the player should be able to choose AI difficulty and which player they play using check boxes and dropdown boxes, 35:The easy AI should build a tree 1 ply deep and choose the highest ranked branch, 36:The medium AI should build a tree 3 plies deep and choose the highest ranked branch, 37:The hard AI should build a tree 5 plies deep and choose according to the highest ranked branch

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 from enum import auto
+from random import randint
 
 class Game:
 
@@ -8,8 +9,18 @@ class Game:
     #Using enum.auto() allows me to generate an inconsequential value for a draw
     VTCL = '|'
     HZTL = '--'
+    AI = 'AI'
+    EASY = 1
+    MED = 3
+    HARD = 5
+    #These code constants (EASY-PFCT) are for the AI difficulty; how many layers (or plies) deep does the minimax algorithm go
+    LEFT = 'l'
+    RIGHT = 'r'
+    UP = 'u'
+    DOWN = 'd'
+    # These last few code constants are used instead of literal constants wen checking box capture
 
-    def __init__(self,dim,P1,P2):
+    def __init__(self,dim,P1,P2,difficulty=None):
         self._P1 = P1
         self._P2 = P2
         self._dim = dim
@@ -20,12 +31,14 @@ class Game:
         self._player = self._P1
         self._points = {self._P1:0, self._P2:0}
         #Using a dictionary for points makes referencing easier than two separate variables
+        self._difficulty = difficulty
 
     def getP1Name(self):
         return self._P1
 
     def getP2Name(self):
         return self._P2
+    #The two above subroutines are getters for the names of the players
 
     def __repr__(self):
         #This is where the program constructs the printable game board
@@ -55,36 +68,38 @@ class Game:
         if fr[0] == to[0]:
             self._colLines[row][col] = Game.VTCL
             if col < self._dim-1:
-                sides.append('r')
+                sides.append(Game.RIGHT)
             if col > 0:
-                sides.append('l')
+                sides.append(Game.LEFT)
         elif fr[1] == to[1]:
             self._rowLines[row][col] = Game.HZTL
             if row < self._dim-1:
-                sides.append('d')
+                sides.append(Game.DOWN)
             if row > 0:
-                sides.append('u')
-        self.checkBox(row, col, sides)
+                sides.append(Game.UP)
+        if len(sides) > 0:
+            self.checkBox(row, col, sides)
 
     def checkBox(self, row, col, sides):
         #This is where whether a placed line forms a square
         switch = []
         for side in sides:
-            if side == 'r' or side == 'd':
+            if side == Game.RIGHT or side == Game.DOWN:
                 if self._colLines[row][col] != Game.EMPTY and self._colLines[row][col+1] != Game.EMPTY and self._rowLines[row][col] != Game.EMPTY and self._rowLines[row+1][col] != Game.EMPTY:
+                    #It checks if the required lines are filled
                     self._boxes[row][col] = self._player
                     self._points[self._player] += 1
                     switch.append(False)
                 else:
                     switch.append(True)
-            elif side == 'l':
+            elif side == Game.LEFT:
                 if self._colLines[row][col] != Game.EMPTY and self._colLines[row][col-1] != Game.EMPTY and self._rowLines[row][col-1] != Game.EMPTY and self._rowLines[row+1][col-1] != Game.EMPTY:
                     self._boxes[row][col-1] = self._player
                     self._points[self._player] += 1
                     switch.append(False)
                 else:
                     switch.append(True)
-            elif side == 'u':
+            elif side == Game.UP:
                 if self._colLines[row-1][col] != Game.EMPTY and self._colLines[row-1][col+1] != Game.EMPTY and self._rowLines[row][col] != Game.EMPTY and self._rowLines[row-1][col] != Game.EMPTY:
                     self._boxes[row-1][col] = self._player
                     self._points[self._player] += 1
@@ -94,10 +109,10 @@ class Game:
         #Deciding whether to switch whose turn it is or not
         if len(switch) == 1:
             if switch[0]:
-                self._player = self._P1 if self._player == self._P2 else self._P2
+                self.switchTurn()
         else:
-            if switch[0] and switch[1]:
-                self._player = self._P1 if self._player == self._P2 else self._P2
+            if switch[0] and switch[-1]:
+                self.switchTurn()
 
     def getScore(self, player):
         return self._points[player]
@@ -105,14 +120,28 @@ class Game:
     def getDim(self):
         return self._dim
 
+    def getPlayer(self):
+        return self._player
+    #The above 3 subroutines are getters for the specified value
+
+    def isValid(self, fr, to):
+        #To check if a move is valid for the AI
+        try:
+            return not self.occupied(fr,to)
+        except IndexError:
+            return False
+
     def occupied(self, fr, to):
         #This is where the UI class can check if a space is free
-        row = min(to[1], fr[1])
-        col = min(to[0], fr[0])
-        if fr[0] == to[0]:
-            return not self._colLines[row][col] == Game.EMPTY
-        else:
-            return not self._rowLines[row][col] == Game.EMPTY
+        try:
+            row = min(to[1], fr[1])
+            col = min(to[0], fr[0])
+            if fr[0] == to[0]:
+                return not self._colLines[row][col] == Game.EMPTY
+            else:
+                return not self._rowLines[row][col] == Game.EMPTY
+        except IndexError:
+            return True
 
     def at(self, fr, to):
         #This is where the GUI class can display whether there is a line present at a given loaction
@@ -125,8 +154,123 @@ class Game:
 
     def boxOwner(self,coord):
         #This is where the GUI class can check who has captured a box
-        print(coord[0],coord[1],self._boxes[coord[0]][coord[1]])
         return self._boxes[coord[0]][coord[1]]
+
+    def switchTurn(self):
+        #Switching turns is used by many different classes and so I deemed it needed its own subroutine
+        self._player = self._P1 if self._player == self._P2 else self._P2
+
+    def getInfo(self):
+        return [[list(i) for i in self._rowLines],[list(i) for i in self._colLines],self._player,{k:v for (k,v) in self._points.items()},self._dim,self._P1,self._P2]
+
+    def ai(self):
+        if self._difficulty == Game.EASY:
+            fr = [50,50]
+            to = [50,50]
+            while not self.isValid(fr,to):
+                row = randint(0,self._dim)
+                col = randint(0,self._dim)
+                fr = [row,col]
+                orientation = randint(1,2)
+                if orientation == 1:
+                    to = [row+1,col]
+                else:
+                    to = [row,col+1]
+            return fr,to
+        else:
+            val, fr, to = self.__max(0,-100,100,self.getInfo())
+            return fr,to
+
+    def __max(self,plies,alpha,beta,info):
+
+        tG = tempGame(info)
+
+        maxv = -100
+        minv = 100
+        pFr = None
+        pTo = None
+
+        if plies == self._difficulty:
+            return tG.getScore(), 0, 0
+
+        for row in range(self._dim):
+            for col in range(self._dim):
+                for orientation in [Game.VTCL,Game.HZTL]:
+                    fr = [row,col]
+                    to = [row+1,col] if orientation == Game.VTCL else [row,col+1]
+                    if tG.isValid(fr,to):
+                        tG.play(fr,to)
+                        if tG.getPlayer() == Game.AI:
+                            m, minFr, minTo = self.__max(plies+1,alpha,beta,tG.getInfo())
+                            if m < minv:
+                                minv = m
+                                pFr = fr
+                                pTo = to
+                            if minv <= alpha:     
+                                return minv, pFr, pTo
+
+                            if minv < beta:
+                                beta = minv
+                        else:
+                            m, minFr, minTo = self.__min(plies+1,alpha,beta,tG.getInfo())
+                            if m > maxv:
+                                maxv = m
+                                pFr = fr
+                                pTo = to
+
+                            if maxv >= beta:
+                                return maxv, pFr, pTo
+
+                            if maxv > alpha:
+                                alpha = maxv
+
+        return maxv, pFr, pTo
+
+    def __min(self,plies,alpha,beta,info):
+
+        tG = tempGame(info)
+
+        minv = 100
+        maxv = -100
+        qFr = None
+        qTo = None
+
+        if plies == self._difficulty:
+            return tG.getScore(), 0, 0
+
+        for row in range(self._dim):
+            for col in range(self._dim):
+                for orientation in [Game.VTCL,Game.HZTL]:
+                    fr = [row,col]
+                    to = [row+1,col] if orientation == Game.VTCL else [row,col+1]
+                    if tG.isValid(fr,to):
+                        tG.play(fr,to)
+                        if tG.getPlayer() != Game.AI:
+                            m, maxFr, maxTo = self.__min(plies+1,alpha,beta,tG.getInfo())
+                            if m > maxv:
+                                maxv = m
+                                qFr = fr
+                                qTo = to
+
+                            if maxv >= beta:
+                                return maxv, qFr, qTo
+
+                            if maxv > alpha:
+                                alpha = maxv
+                        else:
+                            m, maxFr, maxTo = self.__max(plies+1,alpha,beta,tG.getInfo())
+                            if m < minv:
+                                minv = m
+                                qFr = fr
+                                qTo = to
+                            if minv <= alpha:     
+                                return minv, qFr, qTo
+
+                            if minv < beta:
+                                beta = minv
+
+        return minv, qFr, qTo
+
 
     @property
     def winner(self):
@@ -151,6 +295,22 @@ class Game:
         elif self._points[self._P1] < self._points[self._P2]:
             return self._P2
         return Game.DRAW
+
+
+class tempGame(Game):
+
+    MIN = 'min'
+    MAX = 'max'
+
+    def __init__(self, info):
+        super(tempGame, self).__init__(info[4], info[5], info[6])
+        self._rowLines = info[0]
+        self._colLines = info[1]
+        self._player = info[2]
+        self._points = info[3]
+
+    def getScore(self):
+        return self._points[Game.AI] - self._points[self._P1] if self._player == self._P2 else self._points[Game.AI] - self._points[self._P2]
 
 if __name__ == '__main__':
     # For unit testing
--- UI.py
+++ UI.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from Game import Game
-from tkinter import Button, Tk, Toplevel, Frame, N,S,E,W,X,Y, LEFT,RIGHT, END, Scrollbar, Text, Message, Grid, StringVar, Label
+from tkinter import Button, Tk, Toplevel, Frame, N,S,E,W,X,Y, LEFT,RIGHT, END, Scrollbar, Text, Message, Grid, StringVar, Label, OptionMenu
 
 class Ui(ABC):
     @abstractmethod
@@ -8,14 +8,17 @@ class Ui(ABC):
         raise NotImplementedError
 
 class Gui(Ui):
+    GUI = 'GUI'
+
     def __init__(self):
+        #Setting up the main window
         self._gameWin = None
         root = Tk()
         root.title('Dots and Boxes')
         frame = Frame(root)
         frame.pack(fill=X)
         Button(frame,text='Help',command=self.helpCallback).pack(fill=X)
-        Button(frame,text='Play Game',command=self.playCallback).pack(fill=X)
+        Button(frame,text='Play Game',command=self.optionsWinSetup).pack(fill=X)
         Button(frame,text='Quit',command=root.quit).pack(fill=X)
         console = Text(frame, height=4, width=50)
         scroll = Scrollbar(frame)
@@ -27,13 +30,69 @@ class Gui(Ui):
         self._root = root
 
     def helpCallback(self):
-        pass
+        #What happens when the help button is pressed
+        helpWin = Toplevel(self._root)
+        helpWin.title('Help')
+        self._helpWin = helpWin
+        Grid.rowconfigure(helpWin,0,weight=1)
+        Grid.columnconfigure(helpWin,0,weight=1)
+        frame = Frame(helpWin)
+        frame.grid(row=0,column=0,sticky=N+S+E+W)
 
-    def playCallback(self):
+        l = Label(frame,text='Dots and Boxes is a two player game in which players take it in turns to play lines on a grid to make boxes.\nWhoever has captured the most boxes when all of the lines have been played is the winner.\n\nPress on the buttons between the dots to play a line.\nIf you capture a box, you get another turn.')
+        l.grid(row=0,column=0,sticky=N+S+E+W)
+        Grid.rowconfigure(frame,0,weight=1)
+        Grid.columnconfigure(frame,0,weight=1)
+
+        Button(helpWin, text='Dismiss', command=self._helpClose).grid(row=1,column=0)
+
+    def optionsWinSetup(self):
         if self._gameWin:
             return
-        dim = 4
-        self._game = Game(dim,'P1','P2')
+        optionsWin = Toplevel(self._root)
+        optionsWin.title('Options')
+        self._optionsWin = optionsWin
+        Grid.rowconfigure(optionsWin,0,weight=1)
+        Grid.columnconfigure(optionsWin,0,weight=1)
+        frame = Frame(optionsWin)
+        frame.grid(row=0,column=0,sticky=N+S+E+W)
+
+        dims = [3,4,5,6,7,8,9]
+        dim = StringVar()
+        dim.set(dims[1])
+        m = OptionMenu(frame, dim, *dims)
+        m.grid(row=0,column=1,sticky=N+S+E+W)
+        
+        ais = ['None','Player 1', 'Player 2']
+        ai = StringVar()
+        ai.set(ais[0])
+        m = OptionMenu(frame, ai, *ais)
+        m.grid(row=1,column=1,sticky=N+S+E+W)
+
+        Label(frame,text='Board dimension: ').grid(row=0,column=0,sticky=N+S+E+W)
+        Label(frame,text='AI: ').grid(row=1,column=0,sticky=N+S+E+W)
+
+        cmd = lambda : self.closeOptions(dim,ai)
+        Button(optionsWin, text='Play', command=cmd).grid(row=2,column=0)
+        Grid.rowconfigure(frame,0,weight=1)
+        Grid.columnconfigure(frame,0,weight=1)
+
+    def closeOptions(self,dim,ai):
+        self._dim = int(dim.get())
+        self._ai = ai.get()
+        self._optionsWin.destroy()
+        self.playCallback()
+
+
+    def playCallback(self):
+        #What happens when the play button is pressed
+        if self._ai == 'None':
+            self._game = Game(self._dim,'P1','P2')
+        elif self._ai == 'Player 1':
+            self._game = Game(self._dim,Game.AI,'P2')
+        else:
+            self._game = Game(self._dim,'P1',Game.AI)
+
         self._finished = False
         gameWin = Toplevel(self._root)
         gameWin.title('Game')
@@ -49,32 +108,30 @@ class Gui(Ui):
 
         for row in range(0,self._game.getDim()*2,2):
             for col in range(0,self._game.getDim()*2,2):
+                #Putting all of the buttons and labels into the window
                 try:
-                    b = StringVar()
-                    b.set(self._game.at([col//2,row//2],[col//2,row//2+1]))
-                    cmd = lambda r=row//2,c=col//2 : self.playRefresh(r,c,'vtcl')
-                    Button(frame,textvariable=b,command=cmd).grid(row=row+1,column=col if col == 0 else col,sticky=N+S+E+W)
-                    self._colButtons[row//2][col//2] = b
+                    cmd = lambda r=row//2,c=col//2 : self.playRefresh(r,c,Game.VTCL)
+                    self._colButtons[row//2][col//2] = Button(frame,command=cmd,height=10,width=5)
+                    self._colButtons[row//2][col//2].grid(row=row+1,column=col if col == 0 else col,sticky=N+S+E+W)
                 except IndexError:
                     pass
 
                 try:
-                    b = StringVar()
-                    b.set(self._game.at([col//2,row//2],[col//2+1,row//2]))
-                    cmd = lambda r=row//2,c=col//2 : self.playRefresh(r,c,'hztl')
-                    Button(frame,textvariable=b,command=cmd).grid(row=row,column=col+1,sticky=N+S+E+W)
-                    self._rowButtons[row//2][col//2] = b
+                    cmd = lambda r=row//2,c=col//2 : self.playRefresh(r,c,Game.HZTL)
+                    self._rowButtons[row//2][col//2] = Button(frame,command=cmd,height=1,width=20)
+                    self._rowButtons[row//2][col//2].grid(row=row,column=col+1,sticky=N+S+E+W)
                 except IndexError:
                     pass
 
                 l = Label(frame,text='.')
-                l.config(font=('Courier',44))
+                l.config(font=('Courier',37))
                 l.grid(row=row,column=col,sticky=N+S+E+W)
                 
                 try:
                     self._boxOwners[row//2][col//2] = StringVar()
                     self._boxOwners[row//2][col//2].set(self._game.boxOwner([row//2,col//2]))
                     l = Label(frame,textvariable=self._boxOwners[row//2][col//2])
+                    l.config(font=('Courier',40))
                     l.grid(row=row+1,column=col+1,sticky=N+S+E+W)
                 except IndexError:
                     pass
@@ -85,21 +142,64 @@ class Gui(Ui):
 
         Button(gameWin, text='Dismiss', command=self._gameClose).grid(row=1,column=0)
 
+        self.pointWinSetup()
+
+        if self._game.getPlayer() == Game.AI:
+            self._console.insert(END,'AI\'s turn\nThinking...\n')
+            fr, to = self._game.ai()
+            if fr[0] == to[0]:
+                self.playRefresh(fr[0],fr[1],Game.VTCL)
+            else:
+                self.playRefresh(fr[0],fr[1],Game.HZTL)
+
+
+    def pointWinSetup(self):
+        pointsWin = Toplevel(self._root)
+        pointsWin.title('Score')
+        self._pointsWin = pointsWin
+        Grid.rowconfigure(pointsWin,0,weight=1)
+        Grid.columnconfigure(pointsWin,0,weight=1)
+        frame = Frame(pointsWin)
+        frame.grid(row=0,column=0,sticky=N+S+E+W)
+
+        self._points = [None,None]
+
+        self._points[0] = StringVar()
+        self._points[0].set(f'{self._game.getP1Name()}: {self._game.getScore(self._game.getP1Name())}')
+        self._points[1] = StringVar()
+        self._points[1].set(f'{self._game.getP2Name()}: {self._game.getScore(self._game.getP2Name())}')
+        l = Label(frame,textvariable=self._points[0])
+        l.grid(row=1,column=0,sticky=N+S+E+W)
+        l = Label(frame,textvariable=self._points[1])
+        l.grid(row=2,column=0,sticky=N+S+E+W)
+
+        self._turn = StringVar()
+        self._turn.set(f'{self._game.getPlayer()}\'s turn')
+        l = Label(frame,textvariable=self._turn)
+        l.grid(row=0,column=0,sticky=N+S+E+W)
+
+        
+
     def playRefresh(self,row,col,dir):
-        if dir == 'vtcl':
+        #What happens when a line button is pressed
+        if dir == Game.VTCL:
             if not self._game.occupied([col,row],[col,row+1]):
                 self._game.play([col,row],[col,row+1])
-                text = self._game.at([col,row],[col,row+1])
-                self._colButtons[row][col].set(text)
+                self._colButtons[row][col].configure(bg='black')
             else:
-                self._console.insert(END,'Space already occupied\n')
+                self._console.insert(END,'Space already occupied, turn forfeited\n')
+                self._game.switchTurn()
         else:
             if not self._game.occupied([col,row],[col+1,row]):
                 self._game.play([col,row],[col+1,row])
-                text = self._game.at([col,row],[col+1,row])
-                self._rowButtons[row][col].set(text)
+                self._rowButtons[row][col].configure(bg='black')
             else:
-                self._console.insert(END,'Space already occupied\n')
+                self._console.insert(END,'Space already occupied, turn forfeited\n')
+                self._game.switchTurn()
+
+        self._turn.set(f'{self._game.getPlayer()}\'s turn')
+        self._points[0].set(f'{self._game.getP1Name()}: {self._game.getScore(self._game.getP1Name())}')
+        self._points[1].set(f'{self._game.getP2Name()}: {self._game.getScore(self._game.getP2Name())}')
 
         for row in range(len(self._boxOwners)):
             for col in range(len(self._boxOwners[row])):
@@ -111,8 +211,22 @@ class Gui(Ui):
             else:
                 self._console.insert(END,f'The winner was {self._game.winner}\n')
         
+        if self._game.getPlayer() == Game.AI:
+            self._console.insert(END,'AI\'s turn\nThinking...\n')
+            fr, to = self._game.ai()
+            if fr[0] == to[0]:
+                self.playRefresh(fr[0],fr[1],Game.VTCL)
+            else:
+                self.playRefresh(fr[0],fr[1],Game.HZTL)
+        
     def _gameClose(self):
         self._gameWin.destroy()
+        self._pointsWin.destroy()
+
+    def _helpClose(self):
+        self._helpWin.destroy()
+
+    #The above two subroutines are for closing windows when the dismiss button is pressed
                 
     def run(self):
         self._root.mainloop()
@@ -133,33 +247,59 @@ class Terminal(Ui):
                 except ValueError:
                     print('Input isn\'t an integer')
                     continue
-            self._game = Game(dim,'P1','P2')
+            while True:
+                ai = input('Would you like an ai opponent (y/n)?: ').upper()
+                if ai in ['Y','N']:
+                    break
+                print('Not y or n')
+            if ai == 'Y':
+                while True:
+                    try:
+                        choice = int(input('Would you like the AI to play player 1 or 2?: '))
+                        if choice not in [1,2]:
+                            print(f'There is no player {choice}')
+                            continue
+                        else:
+                            if choice == 1:
+                                self._game = Game(dim,'AI', 'P1', Game.HARD)
+                            else:
+                                self._game = Game(dim,'P1','AI', Game.HARD)
+                            break
+                    except ValueError:
+                        print('Not a number')
+                        continue
+            else:
+                self._game = Game(dim,'P1','P2')
             while not self._game.winner:
                 print(self._game)
-                while True:
-                    #Inputting line
-                    fr = input('Enter line start coordinates in the form col,row: ').split(',')
-                    to = input('Enter line end coordinates in the form col,row: ').split(',')
-                    #Validating input
-                    try:
-                        for i in range(2):
-                            #Converting input to integers
-                            fr[i] = int(fr[i])-1
-                            to[i] = int(to[i])-1
-                            if fr[i] > self._game.getDim() or fr[i] < 0 or to[i] > self._game.getDim() or to[i] < 0:
-                                print('Input is not withing board dimensions')
-                                continue
-                    except ValueError:
-                        print('One or more of the inputs weren\'t integers')
-                        continue
-                    if abs(fr[0]-to[0]) > 1 or abs(to[1]-fr[1]) > 1:
-                        print('The line is too long; it should span between two adjacent dots')
-                        continue
-                    if self._game.occupied(fr,to):
-                        print('There is already a line there')
-                        continue
-                    break
-                self._game.play(fr,to)
+                if self._game.getPlayer() == Game.AI:
+                    fr, to = self._game.ai()
+                    self._game.play(fr,to)
+                else:
+                    while True:
+                        #Inputting line
+                        fr = input('Enter line start coordinates in the form col,row: ').split(',')
+                        to = input('Enter line end coordinates in the form col,row: ').split(',')
+                        #Validating input
+                        try:
+                            for i in range(2):
+                                #Converting input to integers
+                                fr[i] = int(fr[i])-1
+                                to[i] = int(to[i])-1
+                                if fr[i] > self._game.getDim() or fr[i] < 0 or to[i] > self._game.getDim() or to[i] < 0:
+                                    print('Input is not withing board dimensions')
+                                    continue
+                        except ValueError:
+                            print('One or more of the inputs weren\'t integers')
+                            continue
+                        if abs(fr[0]-to[0]) > 1 or abs(to[1]-fr[1]) > 1:
+                            print('The line is too long; it should span between two adjacent dots')
+                            continue
+                        if self._game.occupied(fr,to):
+                            print('There is already a line there')
+                            continue
+                        break
+                    self._game.play(fr,to)
             print(self._game)
             #If the game is won, print out the victory message
             if self._game.winner != Game.DRAW:
